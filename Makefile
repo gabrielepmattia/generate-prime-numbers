0d36f8c0aae80b12b440d18865d70b3defe7b093
gabry3795 <gabry.gabry@hotmail.it>
2016-04-14 11:54:32 +0200

    Working files

--- Makefile
+++ Makefile
@@ -1,10 +1,14 @@
 CC = gcc
 CFLAGS = -O2
+LIBS = -lm
 
 default: generate-prime
 
-generate-prime : generate-prime.c
-	$(CC) $(CFLAGS) generate-prime.c -o generate-prime
+isPrime.o: isPrime.c isPrime.h
+	$(CC) $(CFLAGS) $(LIBS) -c isPrime.c
+
+generate-prime : generate-prime.c isPrime.o
+	$(CC) $(CFLAGS) $(LIBS) generate-prime.c isPrime.o -o generate-prime
 
 clean:
 	rm generate-prime
--- generate-prime.c
+++ generate-prime.c
@@ -1,24 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
+#include "isPrime.h"
 
-int generatePrime(int n);
+unsigned long generatePrime(int n);
 double getNanoTime();
+unsigned long fromArrayToInt(char* array, int n);
 
 int main(int argc, char* argv){
-	int prime = generatePrime(10);
-	double time = getNanoTime();
-	printf("nanoseconds::%09ld", time);
+	unsigned long prime = generatePrime(10);
+	printf("\n\nThe prime number is %lu\n\n", prime);
+	//double time = getNanoTime();
+	//printf("nanoseconds::%09ld", time);
 }
 
-int generatePrime(int n){
+unsigned long generatePrime(int n){
 	char* rand_number_array = (char*)malloc(n*sizeof(char));
+	unsigned long prime_test;
+	do {
+		// Generate the random number of n bits
+		// ex. [1,1,0,0,0,0,0,1] = 1100001
+		int i = 0;
+		for(i = 0; i < n; i++){
+			// Last number is always 1
+			if(i == n - 1) { rand_number_array[i] = 1; break; }
+			srand(getNanoTime());
+			rand_number_array[i] = rand() % 2;
+			//printf("number %d\n", rand_number_array[i]);
+		}
+		prime_test = fromArrayToInt(rand_number_array, n);
+	} while(!isPrime(prime_test));
+	return prime_test; 
+}
+
+unsigned long fromArrayToInt(char* array, int n){
+	unsigned long number = 0;
 	int i = 0;
-	for(i = 0; i < n; i++){
-		srand(getNanoTime());
-		rand_number_array[i] = rand() % 2;
-		printf("number %d\n", rand_number_array[i]);
-	}	
+	for(i = n - 1; i >= 0; i--){
+		if(array[i] == 0) continue;
+		number += pow(2, n - i - 1);  
+	}
+	return number;
 }
 
 double getNanoTime(){
